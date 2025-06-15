50e017ae62d84cf103512b0b0bb7436420ada0d3
tjunruh <tanner.unruh@gmail.com>
2025-06-14 23:30:06 -0500

    Create simple version of home screen

---
+++ Code/Renegade_Legion.cpp
@@ -0,0 +1,39 @@
+#include <ascii_engine/frame.h>
+#include <ascii_engine/file_manager.h>
+#include <ascii_engine/label.h>
+#include <ascii_engine/menu.h>
+#include <ascii_engine/ascii_io.h>
+
+int main()
+{
+    ascii_io::ascii_engine_init(true);
+    frame* home_frame = new frame();
+    std::string logo_text = "";
+    file_manager::read_file("text_images/logo.txt", logo_text);
+    label logo(home_frame);
+    logo.set_alignment("center block");
+    logo.enable_word_wrap(false);
+    logo.set_output(logo_text);
+    logo.set_spacing(6, 6, 0, 0);
+    menu initialization_menu(home_frame, "new line");
+    initialization_menu.set_alignment("center block");
+    initialization_menu.append_item("New Game");
+    initialization_menu.append_item("Load Game");
+    initialization_menu.append_item("View Tanks");
+    initialization_menu.append_item("Exit");
+    initialization_menu.build();
+
+    ascii_io::hide_cursor();
+    std::string selection = "";
+    do
+    {
+        home_frame->display();
+        int key_stroke = ascii_io::undefined;
+        initialization_menu.get_selection(selection, key_stroke);
+
+    } while (selection != "Exit");
+
+    delete(home_frame);
+    ascii_io::show_cursor();
+    ascii_io::ascii_engine_end();
+}

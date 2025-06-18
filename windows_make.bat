d5f9b5461b1355eea026a62554b33e15ae838417
tjunruh <tanner.unruh@gmail.com>
2025-06-18 00:21:00 -0500

    Copy all text images to build folder for Windows build

--- windows_make.bat
+++ windows_make.bat
@@ -6,11 +6,10 @@ IF %ERRORLEVEL% NEQ 0 (
 mkdir build
 mkdir build\bin
 mkdir build\bin\board_configs
-mkdir build\bin\text_images
 copy Code\windows_msbuild\Release\Renegade_Legion.exe build\bin\
 copy Code\windows_msbuild\Release\ascii_engine.dll build\bin\
 copy board_configs\* build\bin\board_configs\
-copy text_images\* build\bin\text_images\
+xcopy text_images build\bin\text_images\ /s /e /y
 copy Renegade_Legion.bat build\
 FOR /D %%p IN ("Code\windows_msbuild\*.*") DO rmdir "%%p" /s /q
 
